ed3f8104080d6528ec6767fc1a5f5d4e18019cee
HP Thạnh <hpthanh111@gmail.com>
2018-08-24 13:05:25 +0700

    Add project files.

---
+++ Poke/Poke/DetailPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using PokedexNET;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Poke
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class DetailPage : ContentPage
+	{
+        Pokemon _pokemon;
+
+		public DetailPage (Pokemon pokemon)
+		{
+			InitializeComponent ();
+
+            _pokemon = pokemon;
+
+            var pokedex = new Pokedex();
+            var info = pokedex.GetPokemonInfo(pokemon);
+
+            txtName.Text = info.Names.En;
+            txtDesc.Text = info.PokedexEntries.OmegaRuby.En;
+            txtWeight.Text = info.WeightEu;
+            txtHeight.Text = info.HeightEu;
+            imgImage.Source = ImageSource.FromUri(new Uri(pokemon.ImageUrl));
+
+		}
+
+        private void btnOpenWeb_Clicked(object sender, EventArgs e)
+        {
+            var url = "http://www.pokemon.com/us/pokedex/" + _pokemon.Slug;
+            Device.OpenUri(new Uri(url));
+
+        }
+    }
+}
---
+++ Poke/Poke/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using PokedexNET;
+
+namespace Poke
+{
+	public partial class MainPage : ContentPage
+	{
+        private PokedexNET.Pokedex _pokedex;
+        private Pokemon _pokemon;
+		public MainPage()
+		{
+			InitializeComponent();
+            _pokedex = new Pokedex();
+		}
+
+        private void btnFind_Clicked(object sender, EventArgs e)
+        {
+            var text = txtNumber.Text;
+            if (int.TryParse(text, out int number))
+            {
+                _pokemon = _pokedex.GetPokemon(number);
+                txtName.Text = _pokemon.Name;
+                imgImage.Source = ImageSource.FromUri(new Uri(_pokemon.ImageUrl));
+            }
+        }
+
+        private void btnView_Clicked(object sender, EventArgs e)
+        {
+            if(_pokemon != null)
+            {
+                Navigation.PushAsync(new DetailPage(_pokemon));
+
+            }
+        }
+    }
+}
